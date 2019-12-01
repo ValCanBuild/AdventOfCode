45ceb96366ba8fd6fa8634b966587576162aecf9
Valentin Hinov <val@rockspin.co.uk>
2019-12-01 15:09:15 +0000

    2019 day 1 solution in php

---
+++ 2019/PHP/Day1/day1.php
@@ -0,0 +1,39 @@
+<?php
+
+$inputFile = fopen("input.txt", "r") or die("Unable to open file!");
+$read = fread($inputFile, filesize("input.txt"));
+fclose($inputFile);
+$lines = explode("\n", $read);
+
+// part1($lines);
+part2($lines);
+
+function part1($lines) {
+    $sum = 0;
+    foreach ($lines as $line) {
+        $value = intval($line);
+        $fuel = floor($value / 3) - 2;
+        $sum += $fuel;
+    }
+    
+    echo "Final sum is $sum";
+}
+
+function part2($lines) {
+    $sum = 0;
+    foreach ($lines as $line) {
+        $value = intval($line);
+        $fuel = fuelReq($value);
+        $sum += $fuel;
+    }
+    
+    echo "Final sum is $sum";
+}
+
+function fuelReq(int $mass) {
+    $fuel = floor($mass / 3) - 2;
+    if ($fuel <= 0) {
+        return 0;
+    }
+    return $fuel += fuelReq($fuel);
+}
