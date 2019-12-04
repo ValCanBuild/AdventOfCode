23e691903e2e71464d97cbef941b431263a274b8
Valentin Hinov <val@rockspin.co.uk>
2019-12-04 09:58:10 +0000

    Prepare day5

---
+++ 2019/PHP/Day5/day5.php
@@ -0,0 +1,14 @@
+<?php
+
+$input = file_get_contents("input.txt");
+$lines = explode("\n", $input);
+
+part1($lines);
+echo "\n";
+part2($lines);
+
+function part1($lines) {
+}
+
+function part2($lines) {
+}
